22541889f492b66da9da4df6363632d51f47f237
vincent edeh <vincentedeh42@gmail.com>
2023-10-08 17:24:30 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main 
+
+import(
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+}
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err !=nil {
+		log.Fatal(err)
+	}
+}
